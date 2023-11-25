eca95a591a00f096b88b2a0090b329ead8f8ee58
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-11-26 00:18:59 +0100

    task 1
    MongoDB and Redis

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
+
+from bson import json_util
 from mongoengine import connect, Document, StringField, ReferenceField, ListField, CASCADE
+
 
 connect(db='Go-it_HW8', host="mongodb://localhost:27017")
 
@@ -16,3 +19,8 @@ class Quote(Document):
     tags = ListField(StringField(max_length=15))
     quote = StringField()
     meta = {"collection": "quotes"}
+
+    def to_json(self, *args, **kwargs):
+        data = self.to_mongo(*args, **kwargs)
+        data["author"] = self.author.fullname
+        return json_util.dumps(data, ensure_ascii=False)
